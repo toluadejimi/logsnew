440b22356c1f4a42c2467d15e1884a34a1f27f56
Jimmy <toluadejimi@gmail.com>
2024-04-26 17:21:58 +0100

    bbhhhjj

--- core/resources/views/templates/basic/category_products.blade.php
+++ core/resources/views/templates/basic/category_products.blade.php
@@ -1,43 +1,101 @@
 @extends($activeTemplate . 'layouts.main')
 @section('content')
-
     <div class="container">
-        <section class="catalog-section section-bg py-{{ @$products->count() ? 120 : 60 }}">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-xxl-10 col-xl-11">
-                        <div class="catalog-item-wrapper">
-
-                            <div style="border-radius: 100px; background: #10113D;color: #ffffff;">
-                                <div class="row">
-                                    <div class="col-2"></div>
-                                    <div class="col-3">Product</div>
-                                    <div class="col-5">Price</div>
-                                    <div class="col-2">Stock</div>
-                                </div>
-                            </div>
 
 
-                            @forelse($products as $product)
-                                @include($activeTemplate.'partials/products')
-                            @empty
-                                <div class="empty-data text-center">
-                                    <div class="thumb">
-                                        <img src="{{ asset($activeTemplateTrue . 'images/not-found.png') }}">
-                                    </div>
-                                    <h4 class="title">@lang('No result found for "'.$category->name.'"')</h4>
-                                </div>
-                            @endforelse
-                            {{ paginateLinks($products) }}
-                        </div>
-                    </div>
-                </div>
+
+
+        <div class="card">
+            <div class="card-body">
+
+
+                <table class="table table-sm table-responsive-sm">
+                    <thead style="border-radius: 100px; background: #10113D;color: #ffffff;">
+                        <tr class>
+                            <th style="border-radius: 10px 0px 0px 10px;"></th>
+                            <th>Product</th>
+                            <th>Price</th>
+                            <th></th>
+                            <th style="border-radius: 0px 10px 10px 0px;">Stock</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+
+
+
+                        @forelse ($products as $product)
+                            <tr>
+
+                                <td class="">
+                                    <a href="#" data-help="Click to read detailed description">
+                                        <img src="{{ url('') }}/assets/images/product/{{ $product->image }}"
+                                            height="50" width="50" loading="lazy">
+                                    </a>
+                                </td>
+                                <td class="small col-sm-12">
+                                    <a href="/product/details/{{ $product->id }}">
+                                        {{ \Illuminate\Support\Str::limit($product->name, 50, '...Show more') }}</a>
+                                </td>
+
+                                <td class="small">
+                                    ₦{{ number_format($product->price, 2) }}
+                                </td>
+                                <td>
+
+                                </td>
+                                <td>
+                                    @if ($product->in_stock == 0)
+                                        <div>
+                                            0 pcs.
+                                            <button type="button" class="form-control border-0" type="button"
+                                                data-id="12005">
+                                                <ion-icon class="text-dark" name="bag-add"></ion-icon>
+                                            </button>
+                                        </div>
+                                    @else
+                                        <form action="/product/details/{{ $product->id }}" method="get">
+                                            @csrf
+                                            <div class="button-wrap" onclick="subscribeBuyItem(6);">
+                                                <div data-help="Buy Now">
+                                                    {{ $product->in_stock }} pcs.
+                                                    <button type="submit" class="form-control" type="button"
+                                                        data-id="12005">
+                                                        <ion-icon class="" style="border: 0px;"
+                                                            name="bag-add"></ion-icon>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                </td>
+                        @endif
+                        </tr>
+                    @empty
+                        <td>No product found </td>
+                        @endforelse
+
+
+
+
+                    </tbody>
+
+                    {{ paginateLinks($products) }}
+
+
+                </table>
+
+
             </div>
-        </section>
+        </div>
+
+
+
+
+
+
+
+
+
 
     </div>
-
 @endsection
-
-
-
