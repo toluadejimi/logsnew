43c3032dc78a5492a11ee49b3ecd34388dee4755
Jimmy <toluadejimi@gmail.com>
2024-06-15 00:12:59 +0100

    dep

---
+++ core/resources/views/verify-password.blade.php
@@ -0,0 +1,89 @@
+@extends($activeTemplate . 'layouts.main')
+@section('content')
+
+    <div class="container">
+        <div class="flex">
+
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            @if (session()->has('message'))
+                <div class="alert alert-success">
+                    {{ session()->get('message') }}
+                </div>
+            @endif
+            @if (session()->has('error'))
+                <div class="alert alert-danger">
+                    {{ session()->get('error') }}
+                </div>
+            @endif
+
+
+            <div class="col-lg-6">
+                <div class="card">
+                    <div class="card-body " style="margin-bottom: 100px">
+                        <form action="/reset-password-now" method="POST">
+                            @csrf
+                            @if ($errors->any())
+                                <div class="alert alert-danger">
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
+                            @if (session()->has('message'))
+                                <div class="alert alert-success">
+                                    {{ session()->get('message') }}
+                                </div>
+                            @endif
+                            @if (session()->has('error'))
+                                <div class="alert alert-danger">
+                                    {{ session()->get('error') }}
+                                </div>
+                            @endif
+
+                                <div>
+
+                                    <input name="email" hidden value="{{ $email }}">
+
+                                    <input
+                                        class="form-control"
+                                        id="password" type="password" name="password" placeholder="Enter secured your password" required="required" autofocus="autofocus"
+                                    >
+
+
+
+                                    <input
+                                        class="form-control my-4"
+                                        id="password" type="password" name="password_confirmation" placeholder="Confirm  your password" required="required" autofocus="autofocus"
+                                    >
+                                </div>
+
+
+
+                            <button style="color: white; border-radius: 10px;   background: linear-gradient(279deg, #FF0B9E -6.58%, #FF6501 121.69%);"  class="btn btn-sm my-3" type="submit">Reset</button>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+            <div style="padding-bottom: 700px" class="col-lg-12 mb-90">
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+@endsection
