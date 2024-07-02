a3864f359e369fd86d287950ba105ee2a4d4cf93
Jimmy <toluadejimi@gmail.com>
2024-07-02 09:12:10 +0100

    dep

--- core/resources/views/templates/basic/product_details.blade.php
+++ core/resources/views/templates/basic/product_details.blade.php
@@ -48,7 +48,7 @@
                     </div>
                 @endif
                 <div class="row">
-                    <div class="col-12 d-flex justify-content-center">
+                    <div class="col-12">
 
                         <div class="card">
 
