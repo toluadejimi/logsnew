985eccb9ae0cbeb7c57017effb76cede37c6d1ec
Jimmy <toluadejimi@gmail.com>
2024-11-20 11:07:41 +0100

    dFFF

--- core/app/Http/Controllers/SiteController.php
+++ core/app/Http/Controllers/SiteController.php
@@ -6,6 +6,7 @@ use App\Constants\Status;
 use App\Models\AdminNotification;
 use App\Models\Bought;
 use App\Models\Category;
+use App\Models\Deposit;
 use App\Models\Frontend;
 use App\Models\GatewayCurrency;
 use App\Models\Language;
@@ -404,14 +405,11 @@ class SiteController extends Controller
         return response()->json(['success' => true, 'message' => 'Thank you, we will notice you our latest news']);
     }
 
-    public
-    function e_check(request $request)
-    {
+    public function e_check(request $request){
 
+        $get_user =  User::where('email', $request->email)->first() ?? null;
 
-        $get_user = User::where('email', $request->email)->first() ?? null;
-
-        if ($get_user == null) {
+        if($get_user == null){
 
             return response()->json([
                 'status' => false,
@@ -424,16 +422,15 @@ class SiteController extends Controller
             'status' => true,
             'user' => $get_user->username,
         ]);
+
     }
 
 
-    public
-    function e_fund(request $request)
-    {
+    public function e_fund(request $request){
 
-        $get_user = User::where('email', $request->email)->first() ?? null;
+        $get_user =  User::where('email', $request->email)->first() ?? null;
 
-        if ($get_user == null) {
+        if($get_user == null){
 
             return response()->json([
                 'status' => false,
@@ -445,10 +442,49 @@ class SiteController extends Controller
 
         $amount = number_format($request->amount, 2);
 
+        $get_depo = Deposit::where('trx', $request->order_id)->first() ?? null;
+        if ($get_depo == null){
+            $trx = new Deposit();
+            $trx->trx = $request->order_id;
+            $trx->status = 1;
+            $trx->user_id = $get_user->id;
+            $trx->amount = $request->amount;
+            $trx->method_code = 250;
+            $trx->save();
+        }else{
+            Deposit::where('trx', $request->order_id)->update(['status'=> 1]);
+        }
+
+
+
+
         return response()->json([
             'status' => true,
             'message' => "NGN $amount has been successfully added to your wallet",
         ]);
+
+    }
+
+
+    public function verify_username(request $request)
+    {
+
+        $get_user =  User::where('email', $request->email)->first() ?? null;
+
+        if($get_user == null){
+
+            return response()->json([
+                'username' => "Not Found, Pleas try again"
+            ]);
+
+        }
+
+        return response()->json([
+            'username' => $get_user->username
+        ]);
+
+
+
     }
 
 
--- core/routes/api.php
+++ core/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\Authcontroller;
 use App\Http\Controllers\Api\Categories;
 use App\Http\Controllers\Api\Services;
+use App\Http\Controllers\SiteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
@@ -10,8 +11,8 @@ use App\Http\Controllers\Api\UserController;
 
 
 
-Route::post('e-check',  [UserController::class, 'e_check']);
-Route::post('e-fund',  [UserController::class, 'e_fund']);
+Route::post('e-check',  [SiteController::class, 'e_check']);
+Route::post('e-fund',  [SiteController::class, 'e_fund']);
 Route::post('verify-username',  [UserController::class, 'verify_username']);
 
 
